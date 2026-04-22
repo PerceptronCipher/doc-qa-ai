12795f67398660dce32f2e88f163bd561b0fffef
Adetunji Samuel <125776151+sam-aydev@users.noreply.github.com>
2026-04-22 16:43:53 +0100

    Clean up Navbar.js by removing commented code
    Removed commented-out code for Navbar component.

--- new frontend1/src/Pages/Navbar.js
+++ new frontend1/src/Pages/Navbar.js
@@ -1,32 +1,9 @@
-// import q from "../images/Q&A Ss.png";
-
-// function Navbar() {
-//   return (
-//     <div className="navbar">
-//       <div className="logo">
-//         <img src={q} />
-//       </div>
-//       <div className="nav-links">
-//         <a href="#features">Features</a>
-//         <a href="#who">For Who</a>
-//         <a href="#how">How it Works</a>
-//         <a href="#hero">Documentation</a>
-//       </div>
-//       <div className="nav-btn">
-//         <button>
-//           <a href="#upload" style={{ color: "white", textDecoration: "none" }}>
-//             Upload Docs
-//           </a>
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-// export default Navbar;
 
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import './Navbar.css' 
+
+
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 

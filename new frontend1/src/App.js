df829958904946dc98144dff45981475de48abe4
sam-aydev <samadet0001@gmail.com>
2026-05-05 08:24:19 +0100

    update

--- new frontend1/src/App.js
+++ new frontend1/src/App.js
@@ -6,10 +6,20 @@ import How from "./Pages/How";
 import Features from "./Pages/Features";
 import Who from "./Pages/Who";
 import Footer from "./Pages/Footer";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
     <div className="App1">
+      <Helmet>
+        <title>4pf - AI Document Q&A</title>
+        <meta name="description" content="Welcome to 4pf." />
+        <meta property="og:title" content="Home | 4pf" />
+        <meta
+          property="og:description"
+          content="4pf is an AI Document Q&A is a production-ready tool that allows users to upload PDF documents and ask natural language questions."
+        />
+      </Helmet>
       <div className="App">
         <Navbar />
         <HeroPage />

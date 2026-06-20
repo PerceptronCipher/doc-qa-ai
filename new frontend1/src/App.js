538b4f220b1e37bbe076dfe1bca54521593a86ea
BuildON-Tech <info@buildonhq.org>
2026-06-20 13:08:17 +0100

    update

--- new frontend1/src/App.js
+++ new frontend1/src/App.js
@@ -12,12 +12,12 @@ function App() {
   return (
     <div className="App1">
       <Helmet>
-        <title>4pf - AI Document Q&A</title>
-        <meta name="description" content="Welcome to 4pf." />
-        <meta property="og:title" content="Home | 4pf" />
+        <title>Rader - AI Document Q&A</title>
+        <meta name="description" content="Welcome to Rader." />
+        <meta property="og:title" content="Home | Rader" />
         <meta
           property="og:description"
-          content="4pf is an AI Document Q&A is a production-ready tool that allows users to upload PDF documents and ask natural language questions."
+          content="Rader is an AI Document Q&A is a production-ready tool that allows users to upload PDF documents and ask natural language questions."
         />
       </Helmet>
       <div className="App">
